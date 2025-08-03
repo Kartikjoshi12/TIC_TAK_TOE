5a6c7b03936e7858506bd1d67808762c1bcf7989
kartik joshi <kartikjoshi2208@gmail.com>
2025-08-03 15:14:50 +0530

    add  new tab: comming soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import TicTacToe from "./componets/TicTacToe";
+import Newtab from "./newfeatures/Newtab";
 
 function App() {
   return (
     <>
       <TicTacToe />
+      <Newtab/>
     </>
   );
 }
---
+++ src/newfeatures/Newtab.jsx
@@ -0,0 +1,8 @@
+
+export default function Newtab() {
+  return (
+    <div>
+      <h1>new feature comming soon!</h1>
+    </div>
+  )
+}
